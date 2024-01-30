7a0616c4c1394d4ea79e0cf8ccf773d88246bcc4
jys <dbstjrgudsla@naver.com>
2024-01-30 16:05:31 +0900

    feat 로그인 빼고 구현

--- fitness-web/src/pages/Main.jsx
+++ fitness-web/src/pages/Main.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import Header from '../components/Header';
 import kangaroo from '../img/kangaroo.svg';
 import './Main.css'; // CSS 파일을 import
@@ -7,27 +6,19 @@ import './Main.css'; // CSS 파일을 import
 const Main = () => {
     return (
         <>
-        <Header />
-        <div className="main-container">
-            
-            <div className="Sentence">PT 하지말고<br /> 동네형에게  헬스과외 받자.</div>
-            <div className="WhiteContainer">
-            <div className="white1"></div>
-            <div className="white2"></div>
-            <div className="white3"></div>
-            <div className="white4"></div>
+            <Header />
+            <div className="main-container">
+                
+                <div className="Sentence">PT 하지말고<br /> 동네형에게  헬스과외 받자.</div>
+                <div className="WhiteContainer">
+                    <div className="white1"></div>
+                    <div className="white2"></div>
+                    <div className="white3"></div>
+                    <div className="white4"></div>
+                </div>
+                
+                <img src={kangaroo} alt="Kangaroo Image" className="centered-image2" />
             </div>
-            
-            <img src={kangaroo} alt="Kangaroo Image " className="centered-image2"/>
-=======
-
-const Main = () => {
-    return (
-        <div>
-            
->>>>>>> main
-        </div>
-    
         </>
     );
 };
