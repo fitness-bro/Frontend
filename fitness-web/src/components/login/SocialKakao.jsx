e2a74161f6330f0a7e1b980009c6e7cdb6a4689e
dldusgh318 <pololydotoly@gmail.com>
2024-02-17 23:18:26 +0900

    feat:로그인토큰저장중..(#1)

--- fitness-web/src/components/login/SocialKakao.jsx
+++ fitness-web/src/components/login/SocialKakao.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components"
 import kakaoImg from '../../img/kakaotalk.svg'
 import axios from "axios";
+import { useState,useEffect } from "react";
 
 const Button=styled.button`
 background-color: rgba(0, 0, 0, 0);
@@ -35,11 +36,40 @@ justify-content:center;
 `;
 
 const SocialKakao=()=>{
+  const apiUrl = process.env.REACT_APP_API_URL;
     const Rest_api_key='8c7208b34afec06d51425dd25d6a0a54' //REST API KEY
     const redirect_uri = `https://dev.fitness-bro.pro/login/oauth2/code/kakao` //Redirect URI
-    // oauth 요청 URL
-
     const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`
+    
+    const [token, setToken] = useState();
+
+    useEffect(() => { 
+      const urlParams = new URLSearchParams(window.location.search);
+      const code = urlParams.get('code');
+
+    if (code) {
+      // 코드가 존재하면 토큰 요청을 보냄
+      axios.get(`${apiUrl}/login/oauth2/code/kakao?code=${code}`)
+        .then(response => {
+          const data = response.data;
+          console.log("API 응답:", response);
+
+          if (data.isSuccess) {
+            // 토큰을 추출하여 로컬 스토리지에 저장
+            setToken(data.result.userToken);
+          } else {
+            console.error("API 요청 실패:", data.message);
+          }
+        })
+        .catch(error => {
+          console.error("API 요청 중 오류 발생:", error);
+          console.error("에러 상세 정보:", error.response);
+        });
+    }
+  }, []);
+  
+  localStorage.setItem('token', token);
+
 
     const handleLogin = async () => {
       try {
@@ -54,6 +84,8 @@ const SocialKakao=()=>{
         console.error("로그인 후 처리 중 에러:", error);
         // 에러 처리 로직 추가
       }
+
+      
     };
     return(
     <>
